002f0a90a5777357a57657aa626a51f9ee734dcc
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-02-27 23:58:01 -0500

    update the comments

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-function generatePassword (){
+// generate passowrd
+function generatePassword ()
+{
+  // array to generate random characters
   let alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  let numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   let specialSymbol = ['!','#','$','%','&','*','+','-',':',';','?','@','_','~'];
   
-  let upperCase = true
-  let lowerCase = true
-  let numbers = true
-  let specialChar = true
+  // boolean variables for validating user selection on character types
+  let upperCase   = true;
+  let lowerCase   = true;
+  let numbers     = true;
+  let specialChar = true;
+ 
+  // variables used for password manipulation
+  let upperPassword   = '';
+  let lowerPassword   = '';
+  let specialPassword = '';
+  let numberPassword  = 0 ;
+  let finalpwd        = '';
+  let finalpwdlength  = 0 ;
+  let lastPassword    = ''; 
 
-  let upperPassword = ''
-  let lowerPassword = ''
-  let result = ''
-  let specialPassword = ''
-  let numberPassword = 0
-  let finalpwd = ''
-  let finalpwdlength = 0
-  let lastPassword =''
-
-
+  // prompt to receive lenght of the passowrd
   let passwordLength = prompt('How many characters do you want include in the password?');
-  console.log(passwordLength)
   if (passwordLength >= 8 && passwordLength < 128) {
+    // confrming user selection on character types
       upperCase = confirm('Do you want to use uppercase?');
       lowerCase = confirm('Do you want include lower case?');
       specialChar = confirm('Do you want use special character?');
       numbers = confirm('Do you want use numbers?');
+      // cheack and make sure atlest one character to choose
       if (upperCase===true || lowerCase ===true || specialChar===true || numbers===true)
       {
+           // loop through until the password length is reached,randomly generate each type of character
+           for (let i=0; i<passwordLength;i++){
 
-            for (let i=0; i<passwordLength;i++){
-
-
+             // random generate for upper case           
              if (upperCase) {
                 upperPassword = alpha[Math.floor(Math.random() * alpha.length)];
              }
 
+             // random generate for lower case
              if (lowerCase) {
                 lowerPassword = alpha[Math.floor(Math.random() * alpha.length)];
-                result = lowerPassword.toLowerCase();
+                lowerPassword = lowerPassword.toLowerCase();
              }
 
+             // random generate for special character
              if (specialChar) {
                  specialPassword = specialSymbol[Math.floor(Math.random() * specialSymbol.length)];
              }
 
+             // random generate for numbers
              if (numbers) {
                  numberPassword = Math.floor(Math.random() * 10);
              }
 
-             finalpwd = finalpwd + upperPassword + result + specialPassword + numberPassword ;
-
+             // concatenate each charater for the loop
+             finalpwd = finalpwd + upperPassword + lowerPassword + specialPassword + numberPassword ;
              finalpwdlength=finalpwd.length;
-            
+             
+             // if the concatenate length each loop reaches the maximum length 
+             // extract the exact password length and exit the loop 
              if (finalpwdlength >=passwordLength){
                i=passwordLength;
                lastPassword=finalpwd.substring(0,passwordLength);
@@ -62,14 +71,17 @@ function generatePassword (){
 
             }
           } 
-          else{
+          else{ 
+            // validation message to choose one character
             alert('You must select atleast one character')
           }
         
         } 
         else {
-      alert('password must contain min 8 and max 128');
+          // validation message to choose passowrd length between 8 and 128
+      alert('Password must contain min 8 and max 128');
   }
+  // return the passowrd
   return lastPassword;
 }
 
